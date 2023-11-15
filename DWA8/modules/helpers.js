82469bbd0e8196651361006b59e6e66029738ecd
Malebo29 <shwabilegodi@gmail.com>
2023-11-15 17:06:05 +0200

    created a DWA08 folder and duplicated the todo example files to apply enpsulation

---
+++ DWA8/modules/helpers.js
@@ -0,0 +1,40 @@
+// @ts-check
+
+/**
+ * Descriptions
+ *
+ * @param {string} dataAttr
+ * @param {string} [value]
+ * @returns {HTMLElement}
+ */
+export const getHtml = (dataAttr, value) => {
+  const selector = value
+    ? `[data-${dataAttr}="${value}"]`
+    : `[data-${dataAttr}]`;
+
+  const element = document.querySelector(selector);
+  const isHtmlElement = element instanceof HTMLElement;
+
+  if (!isHtmlElement) {
+    throw new Error(`${selector} attribute not found in HTML `);
+  }
+
+  return element;
+};
+
+/**
+ * Descriptions
+ *
+ * @param {string} dataAttr
+ * @param {string} [value]
+ * @returns {boolean}
+ */
+export const doesHtmlExist = (dataAttr, value) => {
+  const selector = value
+    ? `[data-${dataAttr}="${value}"]`
+    : `[data-${dataAttr}]`;
+
+  const element = document.querySelector(selector);
+  const isHtmlElement = element instanceof HTMLElement;
+  return isHtmlElement;
+};
